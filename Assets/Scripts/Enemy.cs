31029e1b847f2b1541647f44bc99ec004a6921b8
Gen0902 <dambraij@gmail.com>
2020-10-09 13:21:07 +0200

    Add project files.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    private Vector2 target;
+    int wayPointIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = NodeManager.Instance.wayPoints[0];
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
+        Vector3 dir = (Vector3)target - transform.position;
+
+        transform.rotation = Quaternion.LookRotation(Vector2.Lerp(transform.right, dir, Time.fixedDeltaTime));
+
+        //transform.rotation = Quaternion.LookRotation(dir.normalized * speed * Time.deltaTime,-Vector3.forward);
+
+        if (transform.position == (Vector3)target && wayPointIndex < NodeManager.Instance.wayPoints.Count - 1)
+        {
+            target = NodeManager.Instance.wayPoints[++wayPointIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour
+{
+
+    public static NodeManager Instance;
+
+    public GameObject waypointsParent;
+
+    [HideInInspector]
+    public List<Vector2> wayPoints;
+    private Transform[] wayPointsTr;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("More than one instance of NodeManager found !");
+            return;
+        }
+        Instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wayPointsTr = waypointsParent.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < wayPointsTr.Length; i++)
+        {
+            if (wayPointsTr[i] != waypointsParent.transform)
+                wayPoints.Add(wayPointsTr[i].position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
